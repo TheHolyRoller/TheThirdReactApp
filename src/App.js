f08f33d04604871c357495b57ab454575689a9a0
Danny <danielwakeley7@gmail.com>
2022-09-26 21:09:30 +0300

    Just upadating things

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Home from "./pages/Home";
+import Pages from "./pages/Pages";
+
 
 //PA Token
 //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
@@ -7,20 +10,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> Welcome </h1>
+    <Pages />
+
+
     </div>
   );
 }
---
+++ src/components/Veggie.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Veggie() {
+  return (
+    <div>Veggie</div>
+  )
+}
+
+export default Veggie
