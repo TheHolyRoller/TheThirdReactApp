c747eb935e0c4b790675b250ae6b860022bd6d5c
Danny <danielwakeley7@gmail.com>
2022-09-27 09:05:33 +0300

    Removed some things from .env and Popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 
 function Popular() {
+
+  const [popular, setPopular] = useState([]);
+
+
+useEffect(() => {
+
+getPopular();
+
+
+}, []);
+
+
+
+
+  const getPopular = async () => {
+
+    //TO DO  SORT OUT API KEY IN .ENV
+    //const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process}&number=9`);
+    //const data = await api.json();
+    //console.log(data);
+    //setPopular(data.recipes)
+
+  }
+
+
   return (
-    <div>Popular</div>
+    <div>
+
+      <div>
+      <p>
+      Hellow
+      </p>
+      </div>
+
+      );
+
+    }
+
+
+    </div>
+
+
+
+
+
   )
 }
 
